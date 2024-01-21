cd712943eeebc1985b7fe3b73cd74feb8826e83a
Harry <indsingh0505@gmail.com>
2024-01-21 11:40:23 +0530

    feat: Added files for variables and datatype notest

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+//there are three types of variable declarations in javascript var, let, const
+//var was the older approach of declaring variable
+//Since var was creating issues related to scope of the variable let and const came into the picture
+
+
+const accountId = 13232;
+let accountEmail = 'akash@gmail.com'
+var accountPassword = 'akash123'
+accountCity = 'Chirmiri'
+
+//accountId = 34244 // is not allowed
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+//number => 2 to power of 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+
+//object
+let state = true;
+
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof state);
